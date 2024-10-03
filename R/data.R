debd5924123f2c3a188d13d4e3c64ee3c236fe12
Sean McGrath <smcgrath@hsph.harvard.edu>
2024-10-03 00:43:44 -0400

    Added documentation for the example data set

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Example dataset with null treatment effects
+#'
+#' A dataset consisting of 25,000 observations on 1,000 individuals over 25 time points. Each row in the dataset corresponds to the record of one individual at one time point.
+#'
+#' @docType data
+#'
+#' @format A data table with 25,000 rows and 7 variables:
+#' \describe{
+#'   \item{t0}{Time index.}
+#'   \item{id}{Unique identifier for each individual.}
+#'   \item{L}{Binary covariate.}
+#'   \item{L0}{Baseline value of \code{L}.}
+#'   \item{Z}{Medication initiated at baseline.}
+#'   \item{A}{Binary indicator of adhering to medication initiated at baseline.}
+#'   \item{R}{Indicator if the outcome of interest is measured.}
+#'   \item{Y}{Continuous outcome of interest.}
+#'   \item{C_artifial}{Indicator whether an individual is artificially censored.}
+#'   \item{G}{Indicator whether an individual is at the end of their grace period.}
+#' }
+"data_null"
